ce515d5ebaed2817cf63a5836251f258af2128d9
Vinsi <vinsimon.thulaseedha@maqta.ae>
2018-11-06 22:19:01 +0400

    corrected Category Text Issue

--- TrolleyAPI/Trolley/Models/Categories.swift
+++ TrolleyAPI/Trolley/Models/Categories.swift
@@ -9,12 +9,12 @@
 import Foundation
 @objc public class CategoryResponseDataModel: NSObject,Codable {
 
-      public  var image     :    String!
+      public  var image     : String?
       public  var categoryId: String!
       public  var parentId  : String!
       public  var ischild   : String!
       public  var name      : String!
-      public  var sortOrder : String!
+      public  var sortOrder : Int!
         private enum CodingKeys: String, CodingKey {
             case image
             case categoryId = "category_id"
@@ -27,12 +27,13 @@ import Foundation
     public required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self) // defining our (keyed) container
         
-       image          =   try container.decode(String.self, forKey: CodingKeys.image      )
-       categoryId     =   try container.decode(String.self, forKey: CodingKeys.categoryId )
-       parentId       =   try container.decode(String.self, forKey: CodingKeys.parentId   )
-       ischild        =   try container.decode(String.self, forKey: CodingKeys.ischild    )
-       name           =   try container.decode(String.self, forKey: CodingKeys.name       ).htmlUnescape()
-       sortOrder      =   try container.decode(String.self, forKey: CodingKeys.sortOrder  )
+       image          =   try? container.decode(String.self, forKey: CodingKeys.image      )
+       categoryId     =   try! container.decode(String.self, forKey: CodingKeys.categoryId )
+       parentId       =   try! container.decode(String.self, forKey: CodingKeys.parentId   )
+       ischild        =   try! container.decode(String.self, forKey: CodingKeys.ischild    )
+       name           =   try! container.decode(String.self, forKey: CodingKeys.name       ).htmlUnescape()
+       let ssortOrder      =   try! container.decode(String.self, forKey: CodingKeys.sortOrder  )
+       sortOrder           = Int(ssortOrder)
         
         
         
